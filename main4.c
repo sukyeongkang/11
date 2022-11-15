e41c806c33a5a7c231a01396252c9472f68333c2
sukyeongkang <sukyeong02@sookmyung.ac.kr>
2022-11-15 16:57:14 +0900

    실습 4

---
+++ main4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int i;
+	int grade[5];
+	int accum = 0;
+	 
+	 for (i=0; i<5;i++)
+	 {
+	 	printf("grade[%i] = ", i);
+	 	scanf("%d", &grade[i]);
+	 }
+	 
+	
+	 
+	 for(i=0; i<5; i++){
+	 	printf( "%i : %i\n", i, *(grade+i));
+	 	accum += *(grade+i);
+	 }
+	 
+	 
+	 printf("average is %i\n", accum/5);
+	return 0;
+}
